eb80216cbe45d69c49112fb881150899a3853d78
galicians <galicians@gmail.com>
2014-09-20 19:54:05 +0100

    class train added

--- lib/train.rb
+++ lib/train.rb
@@ -1,3 +1,12 @@
+require 'passenger_container'
+
 class Train
+	include PassengerContainer
+
+	def initialize
+		@passengers = []
+	end
+
+
 
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -2,7 +2,11 @@ require 'train'
 
 describe Train do
 
+	let(:train) { Train.new }
+	let(:coach) { Coach.new }
 	it 'should be made of a number of coaches' do
-		train = Train.new
+		4.times { train.hold(coach) }
+		expect(train.count).to eq(4)
 	end
+	
 end
