eb26369eb2deb2ff97f81beac352fa22cb1cf648
galicians <galicians@gmail.com>
2014-11-02 21:16:06 +0000

    revisiting faast challenge

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,11 +1,16 @@
 
 class Passenger
 	
-	attr_accessor :origin, :destination
+	attr_accessor :origin, :destination, :money
 
-	def initialize(origin,destination)
+	def initialize(origin ='', destination='')
 		@origin = origin
 		@destination = destination
+		@money = 2
+	end
+
+	def spend_money(charge)
+		@money < charge ? (raise Exception.new("not enougth money")) : @money -= charge
 	end
 	
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -15,6 +15,27 @@ describe Passenger do
 		expect(passenger.destination).not_to eq(nil)
 	end
 
+	it "can accept an origin when created" do
+		pablo = Passenger.new('origin')
+		expect(pablo.origin).to eq 'origin'
+	end
 
+	it "can accept a destination when created" do
+		pablo = Passenger.new('origin','destination')
+		expect(pablo.destination).to eq 'destination'
+	end
+
+	it "will be initialized with money 2" do
+		expect(passenger.money).to eq 2
+	end
+
+	it "will spend money" do
+		passenger.spend_money(2)
+		expect(passenger.money).to eq 0
+	end
+
+	it "will raise an exception if spends more than the current available amount" do
+		expect{ passenger.spend_money(5) }.to raise_exception
+	end
 
 end
